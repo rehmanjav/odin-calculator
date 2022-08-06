d63067f7fa0b12988a368056a66f0f5556117f13
Rehman Javaid <rehmanjav@outlook.com>
2022-08-06 16:45:58 -0400

    Add operator functionality

--- calc.js
+++ calc.js
@@ -2,12 +2,59 @@
 
 // Functions
 
-function squareRoot () {
+function exp () {
+    if (operand1 &&
+        !operand2 &&
+        !operator &&
+        !result) {
+            operator = "x^";
+            updateDisplay(".operator", operator);
 
+    }
 }
 
-function exponent () {
+function add () {
+    if (operand1 &&
+        !operand2 &&
+        !operator &&
+        !result) {
+            operator = "+";
+            updateDisplay(".operator", operator);
 
+    }
+}
+
+function subtract () {
+    if (operand1 &&
+        !operand2 &&
+        !operator &&
+        !result) {
+            operator = "-";
+            updateDisplay(".operator", operator);
+
+    }
+}
+
+function multiply () {
+    if (operand1 &&
+        !operand2 &&
+        !operator &&
+        !result) {
+            operator = "x";
+            updateDisplay(".operator", operator);
+
+    }
+}
+
+function divide () {
+    if (operand1 &&
+        !operand2 &&
+        !operator &&
+        !result) {
+            operator = "÷";
+            updateDisplay(".operator", operator);
+
+    }
 }
 
 function clear () {
@@ -26,7 +73,11 @@ function updateDisplay (display, value) {
     // display should be .operand1 or .operand2 or .operator or .result
     let targetDisplay = document.querySelector(display);
     targetDisplay.textContent = value;
+}
 
+function addListener (target, func) {
+    let button = document.querySelector(target);
+    button.addEventListener('click', func);
 }
 
 function num7 () {
@@ -37,14 +88,18 @@ function num7 () {
             operand1 += "7";
             updateDisplay(".operand1", operand1);
 
+    } else if (operand1 &&
+               !operator && 
+               !operand2 &&
+               !result &&
+               operand1.length < 28) {
+                operand1 += "7";
+                updateDisplay(".operand1", operand1);
     }
 
 }
 
-function addListener (target, func) {
-    let button = document.querySelector(target);
-    button.addEventListener('click', func);
-}
+
 
 
 
@@ -59,4 +114,10 @@ let result = "";
 
 addListener(".num-7", num7);
 addListener(".clear", clear);
+
+addListener(".exp", exp);
+addListener(".add", add);
+addListener(".subtract", subtract);
+addListener(".multiply", multiply);
+addListener(".divide", divide);
 
